f466f50fc58c46c2f7897c7378d4cc50b6817202
I Made Sudana <gateteknologi@gmail.com>
2024-05-10 16:38:42 +0800

    fortify, login, logout

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/', function () {
-    return view('pages.dashboard',['type_menu'=>'']);
+    return view('pages.auth.auth-login');
 });
+// Route::get('/dashboard', function () {
+//     return view('pages.dashboard',['type_menu'=>'']);
+// });
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('home',function(){
+        return view('pages.dashboard',['type_menu' => 'home']);
+    })->name('home');
+});
+
+// Route::get('/login', function () {
+//     return view('pages.auth.auth-login');
+// });
+
